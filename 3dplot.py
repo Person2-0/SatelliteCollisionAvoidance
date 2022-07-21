1651b3cd7e46222f6d18c904aa847672211f3e85
Person2-0 <109387693+Person2-0@users.noreply.github.com>
2022-07-21 14:28:16 -0600

    Update 3dplot.py

--- 3dplot.py
+++ 3dplot.py
@@ -5,16 +5,17 @@ from skyfield.api import load, wgs84
 from skyfield.api import EarthSatellite
 
 
-
 satellite_url = 'file:///home/bella/project/3le.txt'
 satellites = load.tle_file(satellite_url)
-print('Loaded', len(satellites), 'satellites')
+for i in range(len(satellites)):
+    print(satellites[i])
+
 
 
 ts = load.timescale()
-line1 = '1 5U 58002B   22196.28486465  .00000351  00000-0  43722-3 0  9993'
-line2 = '2 5  34.2492  53.6414 1847265 162.8155 204.4542 10.84977341287669'
-satellite = EarthSatellite(line1, line2, 'Vangaurd 1', ts)
+L1 = '1 00005U 58002B   22201.80587978  .00000357  00000-0  43440-3 0  9992'
+L2 = '2 00005  34.2522  36.6233 1847469 187.6731 169.1333 10.84981346288165'
+satellite = EarthSatellite(L1, L2, 'Vangaurd 1', ts)
 print(satellite)
 
 t = ts.now()
@@ -23,15 +24,50 @@ geocentric = satellite.at(t)
 print(geocentric.position.km)
 
 
+Roadster = EarthSatellite(L1, L2)
 
-plt.rcParams["figure.figsize"] = [7.00, 3.50]
-plt.rcParams["figure.autolayout"] = True
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-r = (0.5, -0.5)
-u, v = np.mgrid[0:2 * np.pi:30j, 0:np.pi:20j]
-x = np.cos(u) * np.sin(v)
-y = np.sin(u) * np.sin(v)
-z = np.cos(v)
-ax.plot_surface(x, y, z, color='b')
-plt.show()
+Rpos    = Roadster.at(t).position.km
+Rposecl = Roadster.at(t).ecliptic_position().km
+
+print(Rpos.shape)
+
+halfpi, pi, twopi = [f*np.pi for f in [0.5, 1, 2]]
+degs, rads = 180/pi, pi/180
+
+re = 6378.
+
+theta = np.linspace(0, twopi, 201)
+cth, sth, zth = [f(theta) for f in [np.cos, np.sin, np.zeros_like]]
+lon0 = re*np.vstack((cth, zth, sth))
+lons = []
+for phi in rads*np.arange(0, 180, 15):
+    cph, sph = [f(phi) for f in [np.cos, np.sin]]
+    lon = np.vstack((lon0[0]*cph - lon0[1]*sph,
+                     lon0[1]*cph + lon0[0]*sph,
+                     lon0[2]) )
+    lons.append(lon)
+
+lat0 = re*np.vstack((cth, sth, zth))
+lats = []
+for phi in rads*np.arange(-75, 90, 15):
+    cph, sph = [f(phi) for f in [np.cos, np.sin]]
+    lat = re*np.vstack((cth*cph, sth*cph, zth+sph))
+    lats.append(lat)
+
+if True:
+    fig = plt.figure(figsize=[10, 8])  # [12, 10]
+
+    ax  = fig.add_subplot(1, 1, 1, projection='3d')
+
+
+    x, y, z = Rpos
+    ax.plot(x, y, z)
+    for x, y, z in lons:
+        ax.plot(x, y, z, '-k')
+    for x, y, z in lats:
+        ax.plot(x, y, z, '-k')
+
+    plt.show()
+
+r_Roadster = np.sqrt((Rpos**2).sum(axis=0))
+alt_roadster = r_Roadster - re
